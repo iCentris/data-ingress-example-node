47aaf48d19bec501a06e6c821c405e98e028b6d7
Jon Cain <jon.cain@icentris.com>
2019-08-05 15:36:08 -0600

    Fake(r) order info. Add total to order request

--- src/requests/objects/order.js
+++ src/requests/objects/order.js
@@ -1,3 +1,5 @@
+const faker = require('faker')
+
 module.exports = {
   path: '/data/v1/object/order',
   method: 'POST',
@@ -5,8 +7,9 @@ module.exports = {
   data: () => {
     return JSON.stringify({
       order_id: '456',
-      client_user_id: '1234',
+      client_user_id: '00001',
       order_date: '2018-01-01 04:50:02',
+      //total: "20.00",
       status: {
         id: '1',
         description: 'In Progress'
@@ -15,8 +18,8 @@ module.exports = {
         'z12345',
         'ups6700ke'
       ],
-      personal_volume: 100,
-      commission_volume: 60,
+      personal_volume: 20,
+      commission_volume: 14,
       autoship_template: {
         id: '1',
         next_run_date: '2018-02-01 08:00:00',
@@ -26,12 +29,12 @@ module.exports = {
         }
       },
       items: [{
-        sku: 'sku1234',
-        name: 'Widget 1',
-        description: 'Does something',
+        sku: `sku-${faker.random.alphaNumeric(8)}`,
+        name: faker.commerce.productName(),
+        description: faker.commerce.productAdjective(),
         quantity: 2,
-        unit_price: 35.00,
-        total_price: 70.00,
+        unit_price: 10.00,
+        total_price: 20.00,
         unit_volume: 10.00,
         unit_commission_volume: 7.00,
         total_volume: 20.00,
